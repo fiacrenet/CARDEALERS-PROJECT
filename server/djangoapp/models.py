e6acf0b70a71dfb4bdd56e468ecffa93e5665697
fiacrenet <fiacreyatta@gmail.com>
2024-06-09 04:55:49 -0400

    code optimization

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -14,26 +14,26 @@ class CarMake(models.Model):
 
 
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length = 100)
-    CAR_TYPES = [
+    car_make=models.ForeignKey(CarMake,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    CAR_TYPES=[
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         ('COUPE', 'Coupe'),
         ('CROSSOVER', 'Crossover'),
         ('HATCHBACK', 'Hatchback'),
-    ]
-    type = models.CharField(max_length = 15, choices = CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2024,
-             validators = [
-                 
-                 MaxValueValidator(2024),
-                 MinValueValidator(2015)
+        ]
+    type=models.CharField(max_length=15,choices=CAR_TYPES,default='SUV')
+    year=models.IntegerField(default=2024,
+    validators=[
+      MaxValueValidator(2024),
+      MinValueValidator(2015)
              ])
-
+ 
     def __str__(self):
-        return self.name 
+        return self.name
